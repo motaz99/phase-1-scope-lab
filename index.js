0ba2462fc218e83907fadd0c9dc130d92d179926
motaz <motaz.dn.ali@gmail.com>
2023-03-20 14:28:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+const leastFavoriteCustomer = "Motaz";
+
+var upperCaseCustomerName = () => {
+  customerName = "BOB";
+};
+
+var setBestCustomer = () => {
+  bestCustomer = "not bob";
+};
+
+var overwriteBestCustomer = () => {
+  bestCustomer = "maybe bob";
+};
+
+var changeLeastFavoriteCustomer = () => {
+  leastFavoriteCustomer = "Ahmad";
+};
